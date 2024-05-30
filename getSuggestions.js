0d8531658007a5c19179f49d69be325fe00da4aa
Adhish-Krishna <adhishthesak@gmail.com>
2024-05-30 20:20:52 +0530

    Initial Commit

--- getSuggestions.js
+++ getSuggestions.js
@@ -4,7 +4,6 @@ const getSuggestion = (songId)=>{
     let suggestions = [];
     getSuggestions(songId).then((data)=>{
         data.map((d)=>{
-            //return console.log("Title: "+ d.title+"\n"+"Link: "+`https://music.youtube.com/watch?v=${d.youtubeId}`);
             suggestions = [...suggestions , {Title:d.title,Link:`https://music.youtube.com/watch?v=${d.youtubeId}`}];
         })
         console.log(suggestions);
